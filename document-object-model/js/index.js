455cac4f19818ba22d32d0b6155ae89f7ac3219c
NguyenAnhThanh <anhthanh1996vp@gmail.com>
2018-03-27 17:14:55 +0700

    Calculator-dom
    Calculator

--- document-object-model/js/index.js
+++ document-object-model/js/index.js
@@ -1,31 +1,43 @@
 	
 	// One
-		function changeFontSize(x) {
-			var size = document.getElementsByClassName("content");
-			size[0].style.fontSize = x + "px";
-		}
-	//Two 
-		function increaseFontSize() {
-			var sizeS = document.getElementById("p1");
-			console.log(sizeS.style);
+		function changeFontSize() {
+			var size = document.getElementsByClassName("box");
+			size[0].style.fontSize = "20px";
+			size[1].style.fontSize = "20px";
+			size[2].style.fontSize = "20px";
+
 		}
 
-	//Three
+	//Two 
+	function increaseFontSize() {
+		var sizeS = document.getElementById("p1");
+		if (sizeS.style.fontSize < "30px") {
+			sizeS.style.fontSize = (parseFloat(sizeS.style.fontSize)+1)+'px';
+		}
+	}
+
+	//Three 
+	function decreaseFontSize() {
+		var sizeS = document.getElementById("p2");
+		if (sizeS.style.fontSize > "10px") {
+			sizeS.style.fontSize = (parseFloat(sizeS.style.fontSize)-1)+'px';
+		}
+	}
 
 
 	//Four
-		function changeColor() {
-			var change = document.getElementsByClassName("box");
-			change[0].style.color = "blue";
-			change[1].style.color = "yellow";
-			change[2].style.color = "red";
-		}
+	function changeColor() {
+		var change = document.getElementsByClassName("box");
+		change[0].style.color = "blue";
+		change[1].style.color = "yellow";
+		change[2].style.color = "red";
+	}
 	//Five 
-		function changeBgColor() {
-			var backG = document.getElementsByTagName("body");
-			backG[0].style.backgroundColor = "black";
-		}
+	function changeBgColor() {
+		var backG = document.getElementsByTagName("body");
+		backG[0].style.backgroundColor = "black";
+	}
 	//Six
-		function copyContent() {
-			document.getElementById("p1").innerText = document.getElementById("p2").innerText
-		}
+	function copyContent() {
+		document.getElementById("p1").innerText = document.getElementById("p2").innerText
+	}
