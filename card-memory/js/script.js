68079f845baeac22ddb3052f97256536d42657dd
NguyenAnhThanh <anhthanh1996vp@gmail.com>
2018-04-09 11:57:19 +0700

    finish
    finish

--- card-memory/js/script.js
+++ card-memory/js/script.js
@@ -8,14 +8,26 @@ var score = 0;
 
 $(document).ready(function(){
     loadGame();
+    shuffle();
+    loadGame();
 })
 loadGame = function(){
     themes();
 }
 nextImages = function(){
-  for(var i =1; i <= 12; i++){
+  for(var i = 1; i <= 12; i++){
     arrImages.push('img/'+ i +".jpg",'img/'+ i +".jpg");
 }
+}
+function shuffle() {
+    for (var i = arrImages.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = arrImages[i];
+        arrImages[i] = arrImages[j];
+        arrImages[j] = temp;
+    }
+    return arrImages;
+    console.log(arrImages);
 }
 themes = function(){
     nextImages();
@@ -24,24 +36,19 @@ themes = function(){
         images += "<div class ='card'><img class='back' id = 'back' "+i+" onclick='clickImages(this,"+i+")'; src='img/up.jpg'><img class='front' id='front'"+i+" src='"+arrImages[i]+"'/></div>";
     }
     $('#one').html(images);
-    $('#diemtong').html("</br><span>Điểm "+ score +"</span");
+    // $('#diemtong').html("</br><span>Điểm "+ score +"</span");
 }
 clickImages = function(ctrl, i){
-    console.log($(ctrl));
     if(click == 2) //Nếu đã click lần 2 rồi mà chưa chạy hàm kiểm tra xong thì ko cho chọn ảnh khác
         return;
     if(click == 0 ){
         //Gán vị trí lần chọn
         double1 = i;
         //Gán this từ hàm clickImages
-        ctrl1 = ctrl;
-        //quay ảnh off
-        // console.log($(ctrl + "img"));
+        ctrl1 = ctrl;    
         $(ctrl).addClass("js-back");
         $($(ctrl).next()).addClass("js-front");
-        // $(ctrl1).css('transform', 'rotateY(180deg)');
         //quay ảnh on
-        // $('#front' + double1).css('transform', 'rotateY(0deg)');
         click = 1;
     }else {
         double2 = i;
@@ -51,19 +58,16 @@ clickImages = function(ctrl, i){
         $($(ctrl).next()).addClass("js-front");
         $(ctrl).addClass("js-back");
         $(ctrl1).css('transform', 'rotateY(180deg)');
-        // $(ctrl2).css('transform', 'rotateY(180deg)');
          //quay ảnh off
          $('#front' + double2).css('transform', 'rotateY(0deg)');
          //quay ảnh on
-         click2 = 2;
-         if (ctrl1 != ctrl2) 
-            setTimeout(click, 600);
-        else {
+         click = 0;
+         if (ctrl1 != ctrl2)
+            setTimeout(click, 0.1);
+         else {
             click = 1;
             return;
-        }
-
+        }   
     }
-    console.log(click)
-
+    console.log(ctrl);
 }
