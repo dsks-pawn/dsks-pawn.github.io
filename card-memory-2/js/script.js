fc72b5de7ba3d33261312d03207389ba659aea5d
NguyenAnhThanh <anhthanh1996vp@gmail.com>
2018-04-10 16:41:10 +0700

    finish
    finish

--- card-memory-2/js/script.js
+++ card-memory-2/js/script.js
@@ -5,10 +5,18 @@ var arrImages =[];
 var double1;
 var double2;
 var score = 0;
+var main = document.getElementById('audio-main');
+var front =document.getElementById('audio-front');
+var back = document.getElementById('audio-back');
+var yes =document.getElementById('audio-yes');
+var victory = document.getElementById('audio-victory');
+var lost = document.getElementById('audio-lost');
+
 
 $(document).ready(function(){
     shuffle();
     loadGame();
+    playSound(main);
 })
 loadGame = function(){
     themes();
@@ -32,18 +40,20 @@ themes = function(){
     shuffle();
     var images ='';
     for(var i = 0; i < arrImages.length; i++){
-        images += "<div class ='card'><img class='back' id = 'back ' "+ i +" onclick='clickImages(this,"+i+")'; src='img/up.jpg'>"
-        +"<img class='front' id='front' " + i + " src='"+arrImages[i]+"'/></div>";
+        images += "<div class ='card'><img class='back' id = 'back"+i+"' "+ i +" onclick='clickImages(this,"+i+")'; src='img/up.jpg'>"
+        +"<img class='front' id='front"+i+"' " + i + " src='"+arrImages[i]+"'/></div>";
     }
     $('#one').html(images);
     $('#result').html("</br><span>Điểm "+ score +"</span");
 }
 
 clickImages = function(ctrl, i){
-    // console.log(ctrl)
+    playSound(front)
+   // console.log(ctrl)
     if(click == 2) //Nếu đã click lần 2 rồi mà chưa chạy hàm kiểm tra xong thì ko cho chọn ảnh khác
         return;
     if(click == 0 ){
+
         //Gán vị trí lần chọn
         double1 = i;
         //Gán this từ hàm clickImages
@@ -53,55 +63,59 @@ clickImages = function(ctrl, i){
         //quay ảnh on
         click = 1;
     }else {
-        double2 = i;
+
+     double2 = i;
         //Gán vị trí lần chọn
         ctrl2 = ctrl;
         //Gán this từ hàm clickImages
         $($(ctrl).next()).addClass("js-front");
         $(ctrl).addClass("js-back");
-        $(ctrl1).css('transform', 'rotateY(180deg)');
          //quay ảnh off
-         $('#front' + double2).css('transform', 'rotateY(0deg)');
          //quay ảnh on
+         click = 2;
          if (ctrl1 != ctrl2){
-            setTimeout(checkImages(), 200);
-         }
-         else {
+            setTimeout(checkImages, 1000);
+        }
+        else {
             click = 1;
             return;
-        }   
+        }
     }
-    alert(click);
 }
-
 //Hàm kiểm tra ảnh giống nhau
-function checkImages(double1, double2) {
-    console.log("check");
+function checkImages() {
     //gán lại số lần click = 0
     click = 0;
     // kiểm tra ảnh giống nhau
     if(arrImages[double1] == arrImages[double2]){
-        //ẩn ảnh đi
-        console.log(double1);
-        console(double2);
-        console.log("giống nhau khong?");
-
-        $("#front" + double1).css('display:', 'none');
-        $("#front" + double2).css('display:', 'none');
+        playSound(yes);
+      $("#front"+ double1).css('display', 'none');
+      $("#front"+ double2).css('display', 'none');
         //hiển thị điểm lên giao diện
-        result++;
-        $("#result").html("Điểm " + result); 
-           console("up bai khong?"); 
+        score++;
+        $('#result').html("</br><span>Điểm "+ score +"</span");
     }
-    else {
-        console("up bai khong?");
-        $("#back"+ double1).css('transform', 'rotateY(0deg)');
-        $("#back"+ double2).css('transform', 'rotateY(0deg)');
-        $("#front"+ double1).css('transform', 'rotateY(180deg)');
-        $("#front"+ double2).css('transform', 'rotateY(180deg)');
+    else if (1 != 2) {
+        $("#front" +double1).removeClass("js-front");
+        $("#front" +double2).removeClass("js-front");
+        $("#front" +double2).prev().removeClass("js-back");
+        $("#front" +double1).prev().removeClass("js-back");
+        playSound(back);
     }
     // Kiểm tra thắng
-    if(result == arrImages.length/2){
-        alert('win')
+    if (score == arrImages.length/2){
+        alert('win');
+        playSound(victory);
+        pauseSound(main);
     }
 }
+
+function playSound(id) {
+        id.load(),
+        id.play()
+}
+function pauseSound(id) {
+        id.pause();
+}
+
+
