a3b03db051fd83ec013a38d59b51fae207c3bc2b
akkerise <akkerise@gmail.com>
2018-04-23 10:18:25 +0700

    update

---
+++ project/js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+ 
+$('#itemslider').carousel({ interval: 3000 });
+ 
+$('.carousel-showmanymoveone .item').each(function(){
+var itemToClone = $(this);
+ 
+for (var i=1;i<6;i++) {
+itemToClone = itemToClone.next();
+ 
+if (!itemToClone.length) {
+itemToClone = $(this).siblings(':first');
+}
+ 
+itemToClone.children(':first-child').clone()
+.addClass("cloneditem-"+(i))
+.appendTo($(this));
+}
+});
+});
+
+
+// SCROLL
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip(); 
+  
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+})
