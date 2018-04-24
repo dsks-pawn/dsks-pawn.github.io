4bbb696c6fd86ab9a41b57a4fe13996374a14f60
anhthanh96vp <36871547+anhthanh96vp@users.noreply.github.com>
2018-04-24 17:40:00 +0700

    update
    update

--- project/js/script.js
+++ project/js/script.js
@@ -1,3 +1,4 @@
+// SLIDER
 $(document).ready(function(){
  
 $('#itemslider').carousel({ interval: 3000 });
@@ -48,4 +49,24 @@ $(document).ready(function(){
       });
     } // End if
   });
-})
+});
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
+
+
+
+
