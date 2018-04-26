67481d13e0ac796a0d62d1e6be0b310a85c61443
anhthanh96vp <anhthanh1996vp@gmail.com>
2018-04-27 03:14:08 +0700

    update grill

--- project/js/detaill.js
+++ project/js/detaill.js
@@ -1,22 +1,8 @@
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
-
+$(document).ready(function(){
+	for (var i = 1; i < 21; i++) {
+		$('#number-food').append('<option value = "'+ i +'" >'+ i +'</option>');
+	}
+})
 
 // SLIDER
 $(document).ready(function(){
@@ -39,8 +25,6 @@ itemToClone.children(':first-child').clone()
 }
 });
 });
-//scroll
-
 
 function openMenu(evt, cityName) {
     var i, tabcontent, tablinks;
@@ -55,4 +39,5 @@ function openMenu(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
 document.getElementById("defaultOpen").click();
