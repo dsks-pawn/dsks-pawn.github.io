0527616925832e7d4ffebc90db8803ff28d49cca
anhthanh96vp <anhthanh1996vp@gmail.com>
2018-05-06 04:27:22 +0700

    gulp test

---
+++ application-storage/js/script.js
@@ -0,0 +1,85 @@
+var COLUMN_TYPE =  ['todo', 'doing', 'done'];
+
+var DB = {
+	getData: function() {
+		if (typeof(Storage) !== "undefined") {
+			var data;
+			try {
+				data = JSON.parse(localStorage.getItem('list')) || {};
+			} catch(error) {
+				data = {};
+			}
+			return data;
+		}
+		else {
+			alert("sorry");
+		}
+
+	},
+	setData: function(data) {
+		localStorage.setItem('list', JSON.stringify(data));
+	}
+};
+
+var list = DB.getData();
+
+var app = {
+	newJob: function(e, type) {
+		var event = e;
+		var name = $("#input-" + type).val();
+		if (event.keyCode === 13 && name  !== '') {
+			if ( !list[type] )  list[type] = [];
+			list[type].push(name);
+			DB.setData(list);
+			// update công việc 
+			this.addJob(type, name);
+			// reset input về rỗng
+			$("#input-"+type).val('');
+		}
+	},
+	addJob: function(type, name) {
+		var item = '<a href="#" class="list-group-item bar  list-group-item-danger">' +  name  +' <i onclick="app.deleteJob(this)" class="fas fa-trash-alt"></i></a>';
+		$("#"+ type).append(item);
+	},
+	deleteJob: function(abc) {
+		var modal = $('#Modal');
+		var btn = $('#btn-primary');
+		var item = $(abc).parent();
+		$('#Modal').modal('show');
+
+		btn.off('click');
+
+		btn.on('click', function() {
+			var columnType = item.parent().attr('id');
+			var itemPosition = $('#' + columnType + ' .collection-item').index(item);
+			console.log(itemPosition);
+			
+			item.remove();
+			$('#Modal').modal('hide')
+		})
+	}
+};
+
+
+
+
+
+$( function() {
+	COLUMN_TYPE.forEach( function(type) {
+		var columnType= list [type]  ||  [];
+		columnType.forEach( function(name) {
+			app.addJob(type, name);
+		});
+	});
+	
+	$( ".sort-list" ).sortable({
+		connectWith: ".sort-list",
+		placeholder: 'group-list-item',
+		start:  function (event, ui) {
+			$(ui.item[0]).addClass("dragging");
+		},
+		stop:  function (event, ui) {
+			$(ui.item[0]).removeClass("dragging");
+		}
+	});
+} );
---
+++ gulp-test/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+
+var useref = require('gulp-useref');
+
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+
+var cssnano = require('gulp-cssnano');
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+var imagemin = require('gulp-imagemin');
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'))
+});
